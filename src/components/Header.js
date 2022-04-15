a231e0088acba917bfb552bbc158ae7b7218cb0b
yomZsamora <samaurah@gmail.com>
2022-04-15 22:03:55 +0300

    Done refactoring the Header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-function Header() {
-    return();
+function Header({changeColorMode, isDarkMode}) {
+    return (
+        <header>
+        	<h2>Shopster</h2>
+        	<button onClick={changeColorMode}>
+          		{isDarkMode ? "Dark" : "Light"} Mode
+        	</button>
+      	</header>
+    );
 }
 
 export default Header;
